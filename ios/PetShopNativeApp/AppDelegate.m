23bdaaaa375f129fdc0582f354e164060d62257c
shin donggyu <donggyu9410@gmail.com>
2020-10-01 16:26:48 +0900

    [feature/launchscreen] 새로 생성한 BootSplash.storyboard 설정

--- ios/PetShopNativeApp/AppDelegate.m
+++ ios/PetShopNativeApp/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
+#import "RNBootSplash.h" // <- add the header import React Native BootSplash Module
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -43,6 +45,10 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView]; // <- initialization using the storyboard file name
+
+
   return YES;
 }
 
