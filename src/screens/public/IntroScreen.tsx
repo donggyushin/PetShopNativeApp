6b7146597aad3681e2eabba8b0633366cd0c1647
shin donggyu <donggyu9410@gmail.com>
2020-10-04 01:23:28 +0900

    [feature/intropage] 로그인 회원가입 버튼 생성

--- src/screens/public/IntroScreen.tsx
+++ src/screens/public/IntroScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyledThemeProps} from '../../styles/theme';
+import {View} from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.SafeAreaView`
@@ -8,8 +9,33 @@ const Container = styled.SafeAreaView`
     props.theme.PRIMARY_BACKGROUND_COLOR};
   justify-content: center;
   align-items: center;
+`;
+
+const TopContainer = styled.View`
+  flex: 4;
+  background-color: ${(props: StyledThemeProps) =>
+    props.theme.PRIMARY_BACKGROUND_COLOR};
   width: 100%;
 `;
+const BottomContainer = styled.View`
+  flex: 1;
+  background-color: ${(props: StyledThemeProps) =>
+    props.theme.PRIMARY_BACKGROUND_COLOR};
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.View`
+  background-color: ${(props: StyledThemeProps) =>
+    props.theme.PRIMARY_BUTTON_COLOR};
+  width: 150px;
+  height: 50px;
+  justify-content: center;
+  border-radius: 7px;
+`;
+const Button = styled.Button``;
 
 const TextContainer = styled.View`
   padding: 15px;
@@ -25,12 +51,31 @@ const Title = styled.Text`
   color: ${(props: StyledThemeProps) => props.theme.PRIMARY_TEXT_COLOR};
 `;
 
-const IntroScreen = () => (
-  <Container>
-    <TextContainer>
-      <Title>intro screen</Title>
-    </TextContainer>
-  </Container>
-);
+const IntroScreen = () => {
+  const login = () => {
+    console.log('로그인 버튼 클릭');
+  };
+
+  const newAccount = () => {
+    console.log('회원가입 버튼 클릭');
+  };
+
+  return (
+    <Container>
+      <TopContainer>
+        <Title>Top</Title>
+      </TopContainer>
+      <BottomContainer>
+        <ButtonContainer>
+          <Button color={'white'} onPress={login} title={'로그인'} />
+        </ButtonContainer>
+        <View style={{width: 30}} />
+        <ButtonContainer>
+          <Button color={'white'} onPress={newAccount} title={'회원가입'} />
+        </ButtonContainer>
+      </BottomContainer>
+    </Container>
+  );
+};
 
 export default IntroScreen;
